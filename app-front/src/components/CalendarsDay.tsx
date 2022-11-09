f88096c63695182efacd0c3778aac1ef518e81d4
davy-web <chendavyweb@gmail.com>
2022-11-09 02:56:15 +0100

    style and contentEditable

--- app-front/src/components/CalendarsDay.tsx
+++ app-front/src/components/CalendarsDay.tsx
@@ -7,12 +7,12 @@ type CalendarsDayProps = {
 
 export default function CalendarsDay({ date }: CalendarsDayProps) {
   const calendarTable = [];
-  for (let i = 9; i <= 18; i++) {
-    for (let j = 0; j < 4; j++) {
+  for (let i: number = 9; i <= 18; i++) {
+    for (let j: number = 0; j < 4; j++) {
       calendarTable.push(
         <tr>
           <td className="calendar_time">{i}:{j === 0 ? "00" : j * 15}</td>
-          <td className="calendar_info">...</td>
+          <td className="calendar_info" contentEditable></td>
         </tr>
       );
     }
